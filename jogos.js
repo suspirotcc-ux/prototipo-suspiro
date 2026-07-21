3fdf9707767b80cf5a77bab5c2d9c0f8a9e4707c
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-20 21:35:10 -0300

    Create jogos.js

---
+++ jogos.js
@@ -0,0 +1,23 @@
+const jogos = [
+
+{
+    nome: "Minecraft",
+    capa: "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png"
+},
+
+{
+    nome: "The Sims 4",
+    capa: "https://upload.wikimedia.org/wikipedia/en/b/bf/The_Sims_4_Box_Art.png"
+},
+
+{
+    nome: "Stardew Valley",
+    capa: "https://upload.wikimedia.org/wikipedia/en/f/fd/Logo_of_Stardew_Valley.png"
+},
+
+{
+    nome: "The Last of Us",
+    capa: "https://upload.wikimedia.org/wikipedia/en/8/86/The_Last_of_Us_Box_Art.jpg"
+}
+
+];
