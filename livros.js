98534e8403383ede30107d194748ee96465c2e20
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-20 21:31:56 -0300

    Create livros.js

---
+++ livros.js
@@ -0,0 +1,28 @@
+const livros = [
+
+{
+    nome: "Romeu e Julieta",
+    capa: "https://m.media-amazon.com/images/I/81JjQ4JdA6L.jpg"
+},
+
+{
+    nome: "Harry Potter",
+    capa: "https://m.media-amazon.com/images/I/81iqZ2HHD-L.jpg"
+},
+
+{
+    nome: "Percy Jackson",
+    capa: "https://m.media-amazon.com/images/I/91WN6a6F2KL.jpg"
+},
+
+{
+    nome: "Jogos Vorazes",
+    capa: "https://m.media-amazon.com/images/I/91M9xPIf10L.jpg"
+},
+
+{
+    nome: "Coraline",
+    capa: "https://m.media-amazon.com/images/I/91JIXmdKkKL.jpg"
+}
+
+];
