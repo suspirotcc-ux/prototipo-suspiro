9dee3686f235bdbad1fb0d145e4d84e92016b9e3
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-21 11:29:54 -0300

    Update livros.js

--- livros.js
+++ livros.js
@@ -1,28 +1,33 @@
 const livros = [
 
 {
-    nome: "Romeu e Julieta",
-    capa: "https://m.media-amazon.com/images/I/81JjQ4JdA6L.jpg"
+nome:"Harry Potter",
+capa:"https://m.media-amazon.com/images/I/81iqZ2HHD-L.jpg"
 },
 
 {
-    nome: "Harry Potter",
-    capa: "https://m.media-amazon.com/images/I/81iqZ2HHD-L.jpg"
+nome:"Jogos Vorazes",
+capa:"https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg"
 },
 
 {
-    nome: "Percy Jackson",
-    capa: "https://m.media-amazon.com/images/I/91WN6a6F2KL.jpg"
+nome:"Percy Jackson",
+capa:"https://m.media-amazon.com/images/I/91WN6a6F2KL.jpg"
 },
 
 {
-    nome: "Jogos Vorazes",
-    capa: "https://m.media-amazon.com/images/I/91M9xPIf10L.jpg"
+nome:"Coraline",
+capa:"https://m.media-amazon.com/images/I/91JIXmdKkKL.jpg"
 },
 
 {
-    nome: "Coraline",
-    capa: "https://m.media-amazon.com/images/I/91JIXmdKkKL.jpg"
+nome:"O Hobbit",
+capa:"https://m.media-amazon.com/images/I/91b0C2YNSYL.jpg"
+},
+
+{
+nome:"O Pequeno Príncipe",
+capa:"https://m.media-amazon.com/images/I/81af+Xt6Q6L.jpg"
 }
 
 ];
