645077d44594bafc2edfa0e6f612b57d0eea08c9
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-21 11:30:36 -0300

    Update jogos.js

--- jogos.js
+++ jogos.js
@@ -1,23 +1,23 @@
 const jogos = [
 
 {
-    nome: "Minecraft",
-    capa: "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png"
+nome:"Stardew Valley",
+capa:"https://upload.wikimedia.org/wikipedia/en/f/fd/Logo_of_Stardew_Valley.png"
 },
 
 {
-    nome: "The Sims 4",
-    capa: "https://upload.wikimedia.org/wikipedia/en/b/bf/The_Sims_4_Box_Art.png"
+nome:"Minecraft",
+capa:"https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png"
 },
 
 {
-    nome: "Stardew Valley",
-    capa: "https://upload.wikimedia.org/wikipedia/en/f/fd/Logo_of_Stardew_Valley.png"
+nome:"The Legend of Zelda",
+capa:"https://upload.wikimedia.org/wikipedia/en/0/0d/The_Legend_of_Zelda_Breath_of_the_Wild.jpg"
 },
 
 {
-    nome: "The Last of Us",
-    capa: "https://upload.wikimedia.org/wikipedia/en/8/86/The_Last_of_Us_Box_Art.jpg"
+nome:"Hollow Knight",
+capa:"https://upload.wikimedia.org/wikipedia/en/0/04/Hollow_Knight_first_print_cover.jpg"
 }
 
 ];
