31a8bd658a43f05f4426b0827cf40b3f25103dd1
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-21 11:31:53 -0300

    Update musicas.js

--- musicas.js
+++ musicas.js
@@ -1,28 +1,28 @@
 const musicas = [
 
 {
-    nome: "Spring Day - BTS",
-    capa: "https://upload.wikimedia.org/wikipedia/pt/6/6f/You_Never_Walk_Alone.jpg"
+    nome:"Marina Sena",
+    capa:"https://upload.wikimedia.org/wikipedia/commons/7/7b/Marina_Sena_2023.jpg"
 },
 
 {
-    nome: "Butter - BTS",
-    capa: "https://upload.wikimedia.org/wikipedia/pt/8/8b/BTS_-_Butter.png"
+    nome:"Arctic Monkeys",
+    capa:"https://upload.wikimedia.org/wikipedia/en/b/b5/Arctic_Monkeys_-_AM.png"
 },
 
 {
-    nome: "Dynamite - BTS",
-    capa: "https://upload.wikimedia.org/wikipedia/pt/6/6c/BTS_-_Dynamite.png"
+    nome:"BTS",
+    capa:"https://upload.wikimedia.org/wikipedia/en/8/8d/BTS_-_Map_of_the_Soul_7.png"
 },
 
 {
-    nome: "Fake Love - BTS",
-    capa: "https://upload.wikimedia.org/wikipedia/pt/e/e4/Love_Yourself_Tear.jpg"
+    nome:"Gal Costa",
+    capa:"https://upload.wikimedia.org/wikipedia/commons/1/18/Gal_Costa_2009.jpg"
 },
 
 {
-    nome: "505 - Arctic Monkeys",
-    capa: "https://upload.wikimedia.org/wikipedia/en/8/82/Favourite_Worst_Nightmare.jpg"
+    nome:"Billie Eilish",
+    capa:"https://upload.wikimedia.org/wikipedia/en/9/93/Billie_Eilish_-_Happier_Than_Ever.png"
 }
 
 ];
