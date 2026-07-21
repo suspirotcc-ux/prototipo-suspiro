1f02bbbee157e76ceda2970a66f7ec360e1c7194
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-21 11:47:27 -0300

    Update filmes.js

--- filmes.js
+++ filmes.js
@@ -2,27 +2,27 @@ const filmes = [
 
 {
 nome:"Coraline",
-capa:"https://m.media-amazon.com/images/M/MV5BMjQzNjQzODQ0Nl5BMl5BanBnXkFtZTcwMDAwNzc0MQ@@._V1_.jpg"
+capa:"https://upload.wikimedia.org/wikipedia/en/8/80/Coraline_poster.jpg"
 },
 
 {
 nome:"La La Land",
-capa:"https://m.media-amazon.com/images/M/MV5BMzA5M2U3MzUtZDc2MS00M2U5LTkwOTQtNmQ3YzQ2Y2Y2N2Y3XkEyXkFqcGc@._V1_.jpg"
+capa:"https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png"
+},
+
+{
+nome:"Shrek 2",
+capa:"https://upload.wikimedia.org/wikipedia/en/3/39/Shrek_2_poster.jpg"
+},
+
+{
+nome:"Your Name",
+capa:"https://upload.wikimedia.org/wikipedia/en/0/0b/Your_Name_poster.png"
 },
 
 {
 nome:"Interestelar",
-capa:"https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktYTMwNC00NWI5LWEwNmEtZWFiYzliMjQxNzIxXkEyXkFqcGc@._V1_.jpg"
-},
-
-{
-nome:"Harry Potter",
-capa:"https://m.media-amazon.com/images/I/91x1bW6V7TL.jpg"
-},
-
-{
-nome:"Homem-Aranha no Aranhaverso",
-capa:"https://m.media-amazon.com/images/I/81Y1y7GvJrL.jpg"
+capa:"https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg"
 }
 
 ];
