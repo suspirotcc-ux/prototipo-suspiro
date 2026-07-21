ae18baf1720daecce8db1cd89f8b6109bcc94870
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-20 21:34:18 -0300

    Create musicas.js

---
+++ musicas.js
@@ -0,0 +1,28 @@
+const musicas = [
+
+{
+    nome: "Spring Day - BTS",
+    capa: "https://upload.wikimedia.org/wikipedia/pt/6/6f/You_Never_Walk_Alone.jpg"
+},
+
+{
+    nome: "Butter - BTS",
+    capa: "https://upload.wikimedia.org/wikipedia/pt/8/8b/BTS_-_Butter.png"
+},
+
+{
+    nome: "Dynamite - BTS",
+    capa: "https://upload.wikimedia.org/wikipedia/pt/6/6c/BTS_-_Dynamite.png"
+},
+
+{
+    nome: "Fake Love - BTS",
+    capa: "https://upload.wikimedia.org/wikipedia/pt/e/e4/Love_Yourself_Tear.jpg"
+},
+
+{
+    nome: "505 - Arctic Monkeys",
+    capa: "https://upload.wikimedia.org/wikipedia/en/8/82/Favourite_Worst_Nightmare.jpg"
+}
+
+];
