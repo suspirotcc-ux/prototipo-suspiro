3ec4c328c074ae962ea2d0b8b081aaa764e89cfa
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-21 11:46:22 -0300

    Update musicas.js

--- musicas.js
+++ musicas.js
@@ -1,28 +1,28 @@
 const musicas = [
 
 {
-    nome:"Marina Sena",
-    capa:"https://upload.wikimedia.org/wikipedia/commons/7/7b/Marina_Sena_2023.jpg"
+nome:"Marina Sena",
+capa:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Marina_Sena_2023.jpg/440px-Marina_Sena_2023.jpg"
 },
 
 {
-    nome:"Arctic Monkeys",
-    capa:"https://upload.wikimedia.org/wikipedia/en/b/b5/Arctic_Monkeys_-_AM.png"
+nome:"Arctic Monkeys",
+capa:"https://upload.wikimedia.org/wikipedia/en/b/b5/Arctic_Monkeys_-_AM.png"
 },
 
 {
-    nome:"BTS",
-    capa:"https://upload.wikimedia.org/wikipedia/en/8/8d/BTS_-_Map_of_the_Soul_7.png"
+nome:"BTS",
+capa:"https://upload.wikimedia.org/wikipedia/en/8/8d/BTS_-_Map_of_the_Soul_7.png"
 },
 
 {
-    nome:"Gal Costa",
-    capa:"https://upload.wikimedia.org/wikipedia/commons/1/18/Gal_Costa_2009.jpg"
+nome:"Gal Costa",
+capa:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Gal_Costa_2009.jpg/440px-Gal_Costa_2009.jpg"
 },
 
 {
-    nome:"Billie Eilish",
-    capa:"https://upload.wikimedia.org/wikipedia/en/9/93/Billie_Eilish_-_Happier_Than_Ever.png"
+nome:"Billie Eilish",
+capa:"https://upload.wikimedia.org/wikipedia/en/9/93/Billie_Eilish_-_Happier_Than_Ever.png"
 }
 
 ];
