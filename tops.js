5e3f384ae12a3756c67b8896f4b5e7973f80df18
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-20 21:20:23 -0300

    Update tops.js

--- tops.js
+++ tops.js
@@ -1,1 +1,31 @@
+let categoriaAtual="";
 
+const modalTop=document.getElementById("modalTop");
+
+const pesquisa=document.getElementById("pesquisaTop");
+
+const resultados=document.getElementById("listaResultados");
+
+function abrirModal(tipo){
+
+    categoriaAtual=tipo;
+
+    modalTop.style.display="flex";
+
+    pesquisa.value="";
+
+    mostrarLista("");
+
+}
+
+document.getElementById("fecharTop").onclick=()=>{
+
+    modalTop.style.display="none";
+
+}
+
+pesquisa.onkeyup=()=>{
+
+    mostrarLista(pesquisa.value);
+
+}
