00e24b5e154520713bcae63ef2e7d744aa7022d1
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-21 11:45:43 -0300

    Update livros.js

--- livros.js
+++ livros.js
@@ -2,7 +2,7 @@ const livros = [
 
 {
 nome:"Harry Potter",
-capa:"https://m.media-amazon.com/images/I/81iqZ2HHD-L.jpg"
+capa:"https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg"
 },
 
 {
@@ -12,22 +12,17 @@ capa:"https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg"
 
 {
 nome:"Percy Jackson",
-capa:"https://m.media-amazon.com/images/I/91WN6a6F2KL.jpg"
+capa:"https://upload.wikimedia.org/wikipedia/en/9/9a/The_Lightning_Thief.jpg"
 },
 
 {
 nome:"Coraline",
-capa:"https://m.media-amazon.com/images/I/91JIXmdKkKL.jpg"
+capa:"https://upload.wikimedia.org/wikipedia/en/3/36/Coraline_poster.jpg"
 },
 
 {
 nome:"O Hobbit",
-capa:"https://m.media-amazon.com/images/I/91b0C2YNSYL.jpg"
-},
-
-{
-nome:"O Pequeno Príncipe",
-capa:"https://m.media-amazon.com/images/I/81af+Xt6Q6L.jpg"
+capa:"https://upload.wikimedia.org/wikipedia/en/4/4a/TheHobbit_FirstEdition.jpg"
 }
 
 ];
