96640c4f8b63d485c67235f1b9421fa031596002
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-20 21:30:46 -0300

    Create filmes.js

---
+++ filmes.js
@@ -0,0 +1,28 @@
+const filmes = [
+
+{
+    nome: "Coraline",
+    capa: "https://m.media-amazon.com/images/M/MV5BMjQzNjQzODQ0Nl5BMl5BanBnXkFtZTcwMDAwNzc0MQ@@._V1_.jpg"
+},
+
+{
+    nome: "La La Land",
+    capa: "https://m.media-amazon.com/images/M/MV5BMzA5M2U3MzUtZDc2MS00M2U5LTkwOTQtNmQ3YzQ2Y2Y2N2Y3XkEyXkFqcGc@._V1_.jpg"
+},
+
+{
+    nome: "Shrek 2",
+    capa: "https://m.media-amazon.com/images/M/MV5BN2FkMTRjNWMtYTI5MC00ZjExLTkwNDMtMGE1ZjExYjJhYzQxXkEyXkFqcGc@._V1_.jpg"
+},
+
+{
+    nome: "Your Name",
+    capa: "https://m.media-amazon.com/images/M/MV5BYzA0Zjg0MjgtYjJkMi00MTNhLTlhZTQtODQ0YzI0MWY4MTE2XkEyXkFqcGc@._V1_.jpg"
+},
+
+{
+    nome: "Interestelar",
+    capa: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktYTMwNC00NWI5LWEwNmEtZWFiYzliMjQxNzIxXkEyXkFqcGc@._V1_.jpg"
+}
+
+];
