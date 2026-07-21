6fdef2e19567228e5ad5926ec0e7202662e70942
suspirotcc-ux <suspirotcc@gmail.com>
2026-07-20 21:21:31 -0300

    Update tops.js

--- tops.js
+++ tops.js
@@ -29,3 +29,37 @@ pesquisa.onkeyup=()=>{
     mostrarLista(pesquisa.value);
 
 }
+
+function mostrarLista(texto){
+
+    resultados.innerHTML="";
+
+    let lista=[];
+
+    if(categoriaAtual==="filme") lista=filmes;
+
+    if(categoriaAtual==="livro") lista=livros;
+
+    if(categoriaAtual==="jogo") lista=jogos;
+
+    if(categoriaAtual==="musica") lista=musicas;
+
+    lista
+
+    .filter(item=>item.nome.toLowerCase().includes(texto.toLowerCase()))
+
+    .forEach(item=>{
+
+        resultados.innerHTML+=`
+
+        <div class="resultado">
+
+            ${item.nome}
+
+        </div>
+
+        `;
+
+    });
+
+}
